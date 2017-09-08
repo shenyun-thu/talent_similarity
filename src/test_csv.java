4d10d91970968472ff02acd443893a0387a4e22c
shenyun-thu <shenyun_thu@163.com>
2017-09-08 16:22:32 +0800

    添加了对csv文件的读取和操作 基本实现了大数据量的相似度比较

--- src/test_csv.java
+++ src/test_csv.java
@@ -1,123 +1,109 @@
-
-import jxl.*;
-import jxl.read.biff.BiffException;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 import java.util.regex.Pattern;
 
-
-
-public class ReadExcel {
-
+public class test_csv {
     private String filePath;
-    private List list = new ArrayList();
-    public static int N = 5000000;//最大记录限制
+    public static int N = 5000000;
     public person[] p = new person[N];
-    public person[] p_temp = new person[N];//在获取测试分类信息之后暂时存储相对应的人才信息用于计算
-    private String pattern = "(\\d*)(\\.)(\\d*)";//标识得到的分数
-    public static String[] test_id = new String[10000];
-    public int count;//记录分类后每类中成员的个数
-    public int num_testID;
-    public ReadExcel(String filePath) {
-        this.filePath = filePath;
-    }
+    public person[] p_temp = new person[N];
+    public static String[]  test_id = new String[10000];
+    public int count_person = 0;
+    public int count_temp = 0;
+    public int num_testID = 0;
     public int count_ans_all = 0;
     public static double similarity_limit = 1.5; //相似度的限制
     public static double low_score_limit = 80.0; //各维度平均分最低值限制
     public static double max_minus_limit = 2.0;//各维度上的差值的最大限制
-    
-    
-    private void read_excel() throws IOException,BiffException 
-    {
-        InputStream stream = new FileInputStream(filePath);
-        Workbook rwb = Workbook.getWorkbook(stream);
-        Sheet sheet = rwb.getSheet(0);
-        for(int i = 0;i < sheet.getRows();i++){
-            String[] str = new String[sheet.getColumns()];
-            Cell cell = null;
-            for(int j = 0; j < sheet.getColumns();j++){
-                cell = sheet.getCell(j,i);
-                str[j] = cell.getContents();
-            }
-            list.add(str);
-        }
-    }
 
-    private void outData()
-    {
-        for(int i = 1;i<list.size();i++){
-            int temp = 0;
-            String[] str = (String[])list.get(i);
-            p[i] = new person();
-            p_temp[i] = new person();
-            p[i].person_id = str[0];
-            p[i].test_id = str[7];
-            for(int j = 8;j <str.length;j ++){
-                boolean isMatch  = Pattern.matches(pattern,str[j]);
-                if(isMatch){
-                    p[i].scores[temp] = Double.parseDouble(str[j]);
-                    temp++;
+    test_csv(String filePath){
+        this.filePath = filePath;
+    }
+    
+    public void read_csv(){
+        File csv = new File(filePath);
+        String pattern = "(\\d*)(\\.)(\\d*)";
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader(csv));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        String line = "";
+        try{
+            line = br.readLine();
+            while((line = br.readLine()) != null){
+                int count_s = 0;
+                String[] temp = line.split("\\,");
+                p[count_person] = new person();
+                p[count_person].person_id = temp[0];
+                p[count_person].test_id = temp[7];
+                for(int i = 8; i <temp.length;i++){
+                    boolean isMatch = Pattern.matches(pattern,temp[i]);
+                    if(isMatch){
+                        p[count_person].scores[count_s] = Double.parseDouble(temp[i]);
+                        count_s ++;
+                    }
                 }
+                p[count_person].count = count_s;
+                
+                for(int j = 0;j < p[count_person].count;j++){
+                    p[count_person].average += p[count_person].scores[j];
+                }
+                p[count_person].average /= (p[count_person].count);
+
+                count_person++;
             }
-            p[i].count = temp-1;
-            for(int j = 0;j <= p[i].count;j++){
-                p[i].average += p[i].scores[j];
-            }
-            p[i].average = p[i].average / (p[i].count+1);
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
-
-    private void classify_test(){
+    
+    public void classify_test(){
         num_testID = 0;
         test_id[num_testID] = new String();
-        test_id[num_testID] = p[1].test_id;
+        test_id[num_testID] = p[0].test_id;
         num_testID++;
-        for(int i = 2;i < list.size();i++){
+        for(int i=1;i<count_person;i++){
             boolean flag = true;
             test_id[num_testID] = new String();
-            for(int j=0;j<=num_testID;j++){
+            for(int j = 0;j <= num_testID;j++){
                 if(test_id[j].equals(p[i].test_id)){
                     flag = false;
                     break;
                 }
             }
-            if(flag) {
+            if(flag){
                 test_id[num_testID] = p[i].test_id;
                 num_testID++;
             }
         }
     }
-
     
     public void init_testID(String test_id){
-        count = 0;
-        for(int i = 1;i<list.size();i++){
+        count_temp = 0;
+        for(int i=0;i<count_person;i++){
             if(p[i].test_id.equals(test_id)&&p[i].average > low_score_limit){
-                p_temp[count] = p[i];
-                count ++;
+                p_temp[count_temp] = p[i];
+                count_temp ++;
             }
         }
     }
-
+    
     public void cal_eu_distance(){
         boolean have_ans = false;
         int count_ans = 0;
         double max_score_minus = 0;
-        for(int i = 0;i < count;i++){
-            for(int j = i+1;j < count;j++) {
+        for(int i=0;i<count_temp;i++){
+            for(int j =i+1;j<count_temp;j++){
                 double ans = 0;
                 boolean can_cal = true;
-                for(int k =0; k <= p_temp[i].count;k++){
-                    if((p_temp[i].scores[k] - p_temp[j].scores[k]) > max_score_minus){  
+                for(int k = 0;k <= p_temp[i].count;k++){
+                    if((p_temp[i].scores[k] - p_temp[j].scores[k]) > max_score_minus){
                         max_score_minus = (p_temp[i].scores[k] - p_temp[j].scores[k]);
                         if(max_score_minus > max_minus_limit) {
-                           can_cal = false;
-                           break;
-                       }
+                            can_cal = false;
+                            break;
+                        }
                     }
                 }
                 if(can_cal){
@@ -125,7 +111,7 @@ public class ReadExcel {
                         ans += (p_temp[i].scores[k] - p_temp[j].scores[k])*(p_temp[i].scores[k] - p_temp[j].scores[k]);
                     }
                     ans = Math.sqrt(ans);
-    
+
                     if(ans < similarity_limit){
                         have_ans = true;
                         System.out.println("the distance between id " + p_temp[i].person_id + " and id " + p_temp[j].person_id + " is " + ans);
@@ -149,9 +135,9 @@ public class ReadExcel {
         System.out.println("in this test we find " + count_ans + " groups");
         if(!have_ans) System.out.println("No answer for your request");
     }
-
+    
     public void show(){
-        for(int i = 0;i <num_testID;i++){
+        for(int i=0;i<num_testID;i++){
             init_testID(test_id[i]);
             System.out.println("test_id : "+test_id[i]+" the result of talent_similarity judgment: ");
             System.out.println("-----------------------");
@@ -160,18 +146,26 @@ public class ReadExcel {
         }
         System.out.println("in this tenant we all find "+count_ans_all+" groups");
         System.out.println("in this tenant we classify "+num_testID+" groups");
+        System.out.println("we have " + count_person +" people");
     }
-
-    public static  void main(String[] args) throws  BiffException,IOException {
+    
+    public void print(){
+        for(int i = 0; i < count_person;i++){
+            System.out.print(p[i].person_id + " " + p[i].average+" ");
+            for(int j = 0;j<p[i].count;j++){
+                System.out.print(p[i].scores[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
         long start_time = System.currentTimeMillis();
-        ReadExcel excel = new ReadExcel("C:\\Users\\shenyun\\Desktop\\store\\tenantid_110006.xls");
-        excel.read_excel();
-        excel.outData();
-        excel.classify_test();
-        excel.show();
+        test_csv demo = new test_csv("C:\\Users\\shenyun\\Desktop\\store\\tenantID_110006.csv");
+        demo.read_csv();
+     //   demo.print();
+        demo.classify_test();
+        demo.show();
         long end_time = System.currentTimeMillis();
         System.out.println("程序运行时间为 : "+(end_time - start_time)+" ms");
     }
 }
-   
-    
